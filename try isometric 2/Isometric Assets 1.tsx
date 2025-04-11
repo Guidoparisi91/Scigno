de5c0ad36171337a448e6703528deb74e54b5ea1
Guido p <guidoparisi91@gmail.com>
2025-04-11 19:39:30 -0300

    MapaNuevo
    se agrega mapa hecho en tiled

---
+++ try isometric 2/Isometric Assets 1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Isometric Assets 1" tilewidth="256" tileheight="128" tilecount="200" columns="10">
+ <image source="Isometric Assets 1.png" width="2560" height="2560"/>
+</tileset>
